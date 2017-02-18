2917bbd25d8ce4a5528d7a9139b94d77d2682f52
Dias Nurul Arifin <dias.arifin@gmail.com>
2017-02-18 17:17:31 +0700

    log

--- src/main/java/org/codenergic/akinabot/line/AkinaApplication.java
+++ src/main/java/org/codenergic/akinabot/line/AkinaApplication.java
@@ -6,6 +6,8 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,17 +17,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @LineMessageHandler
 public class AkinaApplication {
+    public static final Logger LOG = LoggerFactory.getLogger(AkinaApplication.class);
     public static void main(String [] args) {
         SpringApplication.run(AkinaApplication.class, args);
     }
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
+        LOG.info("event: {}", event);
         return new TextMessage(event.getMessage().getText());
     }
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
-        System.out.println("event: " + event);
+        LOG.info("event: {}", event);
     }
 }
