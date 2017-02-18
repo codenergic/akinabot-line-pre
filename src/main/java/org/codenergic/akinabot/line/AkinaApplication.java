cb4920b3d90eebcc54207562a316e3402386f947
Dias Nurul Arifin <dias.arifin@gmail.com>
2017-02-18 16:19:17 +0700

    initial commit

---
+++ src/main/java/org/codenergic/akinabot/line/AkinaApplication.java
@@ -0,0 +1,31 @@
+package org.codenergic.akinabot.line;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by diasa on 2/18/17.
+ */
+@SpringBootApplication
+@LineMessageHandler
+public class AkinaApplication {
+    public static void main(String [] args) {
+        SpringApplication.run(AkinaApplication.class, args);
+    }
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+}
