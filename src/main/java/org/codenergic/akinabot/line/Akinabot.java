ede3fbfe156e9ee11cc70ac154eac17ddf07883f
diasa <dias.arifin@asyx.com>
2019-08-13 12:08:54 +0700

    update spring boot

--- src/main/java/org/codenergic/akinabot/line/Akinabot.java
+++ src/main/java/org/codenergic/akinabot/line/Akinabot.java
@@ -2,7 +2,6 @@ package org.codenergic.akinabot.line;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Created by diasa on 2/18/17.
